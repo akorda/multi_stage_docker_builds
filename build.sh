335aaecdfcdc9fcad8ab2e947ba4464d4a32fa2e
akorda <akorda@gmail.com>
2026-03-14 22:36:35 +0200

    build: Verify reproducible build

--- build.sh
+++ build.sh
@@ -1,4 +1,34 @@
-SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
-docker build --target tests.linux-x64 -f Dockerfile --build-arg SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH --output type=tar,dest=tests.linux-x64.tar,rewrite-timestamp=false .
-docker build --target files.linux-x64 -f Dockerfile --build-arg SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH --output type=tar,dest=app.linux-x64.tar,rewrite-timestamp=false .
-docker build --target runtime.linux-x64 -f Dockerfile --build-arg SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH .
+export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
+
+docker build \
+    --target tests.linux-x64 \
+    -f Dockerfile \
+    --build-arg SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH \
+    --output type=tar,dest=tests.linux-x64.tar,rewrite-timestamp=false .
+
+docker build \
+    --target files.linux-x64 \
+    -f Dockerfile \
+    --build-arg SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH \
+    --output type=tar,dest=app.linux-x64.tar,rewrite-timestamp=false \
+    .
+
+docker build \
+    --no-cache \
+    --target runtime.linux-x64 \
+    -f Dockerfile \
+    --build-arg SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH \
+    --provenance=false \
+    --sbom=false \
+    -t tag1 \
+    .
+
+docker build \
+    --no-cache \
+    --target runtime.linux-x64 \
+    -f Dockerfile \
+    --build-arg SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH \
+    --provenance=false \
+    --sbom=false \
+    -t tag2 \
+    .
