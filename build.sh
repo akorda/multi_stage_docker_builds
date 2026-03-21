2587189604973a3a9c3be470160b1a4507e4fb49
akorda <akorda@gmail.com>
2026-03-21 23:20:11 +0200

    build & verify the images locally

--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
 export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
 
-docker build \
-    --target verify_dotnet_format \
-    -f Dockerfile \
-    --build-arg SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH \
-    .
+# docker build \
+#     --target verify_dotnet_format \
+#     -f Dockerfile \
+#     --build-arg SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH \
+#     .
 
-docker build \
-    --target tests.linux-x64 \
-    -f Dockerfile \
-    --build-arg SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH \
-    --output type=tar,dest=tests.linux-x64.tar,rewrite-timestamp=false .
+# docker build \
+#     --target tests.linux-x64 \
+#     -f Dockerfile \
+#     --build-arg SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH \
+#     --output type=tar,dest=tests.linux-x64.tar,rewrite-timestamp=false .
 
-docker build \
-    --target files.linux-x64 \
-    -f Dockerfile \
-    --build-arg SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH \
-    --output type=tar,dest=app.linux-x64.tar,rewrite-timestamp=false \
-    .
+# docker build \
+#     --target files.linux-x64 \
+#     -f Dockerfile \
+#     --build-arg SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH \
+#     --output type=tar,dest=app.linux-x64.tar,rewrite-timestamp=false \
+#     .
 
 docker build \
     --no-cache \
@@ -39,3 +39,7 @@ docker build \
     --sbom=false \
     -t tag2 \
     .
+
+# wget -q -O diffoci https://github.com/reproducible-containers/diffoci/releases/download/v0.1.8/diffoci-v0.1.8.linux-amd64
+# chmod +x ./diffoci
+./diffoci diff --semantic docker://tag1 docker://tag2
